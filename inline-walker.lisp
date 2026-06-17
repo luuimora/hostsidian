69dff06afadf8faccd7a1717c4fbaa8dfe41fd6c
luuimora <151864001+luuimora@users.noreply.github.com>
2026-06-17 16:07:03 +0300

    Lisence

--- inline-walker.lisp
+++ inline-walker.lisp
@@ -1,3 +1,20 @@
+;;
+;; Websidian. Converts .md to .html
+;; Copyright (C) 2026  Artyom Gorlov
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
 (in-package :websidian)
 (defun inline-walker (line) "Uses the LOOP macro to scan a line and identify Markdown inline syntax."
   (let ((result (make-string-output-stream))
